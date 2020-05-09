f74e6e6439ebb5e2de0ab05fa698599057f1db17
Tu Do <doanhtu07@gmail.com>
2020-05-08 19:42:35 -0700

    test

--- back-end/index.js
+++ back-end/index.js
@@ -35,7 +35,7 @@ setupPassport(passport);
 app.use((req, res, next) => {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'/*, 'https://obscure-badlands-88487.herokuapp.com/'*/);
+    res.setHeader('Access-Control-Allow-Origin', `${process.env.FRONTEND_HOST}`);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
