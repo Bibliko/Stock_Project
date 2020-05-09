b3d906e3133dc709528a5c02647442b9237b6da1
Tu Do <doanhtu07@gmail.com>
2020-05-09 15:14:07 -0700

    test

--- back-end/index.js
+++ back-end/index.js
@@ -31,7 +31,7 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        domain: 'minecommand.us',
+        domain: 'api.minecommand.us',
         secure: true
     }
 }));
