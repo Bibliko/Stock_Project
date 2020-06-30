a486248ef2773e9b8a0bf87bc2d10f7bfcac3f8a
Tu Do <doanhtu07@gmail.com>
2020-06-29 22:15:00 -0500

    Minor changes (3)

--- front-end/src/pages/Login/Login.js
+++ front-end/src/pages/Login/Login.js
@@ -129,21 +129,7 @@ const styles = theme => ({
         fontWeight: 500,
         "& label.Mui-focused": {
             color: "black"
-          },
-        "& .MuiInput-underline:after": {
-            borderBottomColor: 'black'
-          },
-        '& .MuiOutlinedInput-root': {
-            '& fieldset': {
-              borderColor: 'black',
-            },
-            '&:hover fieldset': {
-              borderColor: 'black',
-            },
-            '&.Mui-focused fieldset': {
-              borderColor: 'black',
-            },
-          },
+        },
     }
 });
 
