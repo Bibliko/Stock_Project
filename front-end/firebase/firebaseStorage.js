d66f3793c7d92dc409f7b7d30daf47b92d4432cf
Tu Do <doanhtu07@gmail.com>
2020-07-27 16:57:14 -0500

    Minor fix

--- front-end/firebase/firebaseStorage.js
+++ front-end/firebase/firebaseStorage.js
@@ -1,13 +1,10 @@
-try {
-    require("dotenv").config();
-} catch(error) {
-    console.log("No .env found. Using default ENV.");
-}
+require("dotenv").config();
+const { isUndefined } = require('lodash');
 
 global.XMLHttpRequest = require("xhr2");
 var firebase = require("firebase");
 
-// // Add the Firebase products that you want to use
+// Add the Firebase products that you want to use
 require("firebase/auth");
 require("firebase/storage");
 
@@ -18,6 +15,10 @@ const {
     REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
     REACT_APP_FIREBASE_DATABASE_URL: databaseURL,
 } = process.env;
+
+if(isUndefined(apiKey) || isUndefined(storageBucket) || isUndefined(projectID) || isUndefined(authDomain) || isUndefined(databaseURL)) {
+    throw "No config for Firebase yet.";
+}
 
 var firebaseConfig = {
     apiKey,
