8424e82096736d0d379abc591af8936c364143bc
Legacy107 <49741920+Legacy107@users.noreply.github.com>
2020-07-31 20:51:28 +0700

    Formatted and added min-width to settingTextFields

--- front-end/src/components/TextField/SettingNormalTextField.js
+++ front-end/src/components/TextField/SettingNormalTextField.js
@@ -11,108 +11,109 @@ import IconButton from "@material-ui/core/IconButton";
 import EditRoundedIcon from "@material-ui/icons/EditRounded";
 
 const styles = theme => ({
-  textFieldContainer: {
-    marginLeft: "1.75%",
-    marginRight: "1.75%",
-  },
-  textField: {
-    width: '100%',
-    marginTop: "5px",
-    fontWeight: "normal",
-    "& label.Mui-focused": {
-      color: "black"
+    textFieldContainer: {
+        minWidth: '150px',
+        marginLeft: "1.75%",
+        marginRight: "1.75%",
     },
-    "& .MuiFilledInput-underline:after": {
-      borderBottom: "2px solid #000000"
+    textField: {
+        width: '100%',
+        marginTop: "5px",
+        fontWeight: "normal",
+        "& label.Mui-focused": {
+            color: "black"
+        },
+        "& .MuiFilledInput-underline:after": {
+            borderBottom: "2px solid #000000"
+        },
+        "& .MuiFilledInput-root": {
+            "&.Mui-focused": {
+                backgroundColor: "rgba(225,225,225,0.5)"
+            }
+        }
     },
-    "& .MuiFilledInput-root": {
-      "&.Mui-focused": {
-        backgroundColor: "rgba(225,225,225,0.5)"
-      }
-    }
-  },
-  input: {
-    color: "black",
-    backgroundColor: "rgba(225,225,225,0.65)",
-    "&:hover": {
-      backgroundColor: "rgba(225,225,225,0.5)"
+    input: {
+        color: "black",
+        backgroundColor: "rgba(225,225,225,0.65)",
+        "&:hover": {
+            backgroundColor: "rgba(225,225,225,0.5)"
+        },
+        "& input": {
+            backgroundColor: "rgba(225,225,225,0)"
+        },
+        fontSize: 'medium',
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 'small',
+            height: '40px'
+        },
     },
-    "& input": {
-      backgroundColor: "rgba(225,225,225,0)"
+    title: {
+        fontSize: '20px',
+        [theme.breakpoints.down('xs')]: {
+            fontSize: '15px'
+        },
+        paddingLeft: '5px',
+        fontWeight: "bold",
     },
-    fontSize: 'medium',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 'small',
-      height: '40px'
-    },
-  },
-  title: {
-    fontSize: '20px',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '15px'
-    },
-    paddingLeft: '3%',
-    fontWeight: "bold",
-  },
 });
 
 class SettingNormalTextField extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          onHover: false
-      };
-      this.onMouseOver = this.onMouseOver.bind(this);
-      this.onMouseOut = this.onMouseOut.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            onHover: false
+        };
+        this.onMouseOver = this.onMouseOver.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+    }
 
-  onMouseOver () {
-      this.setState({
-          onHover: true
-      });
-  }
+    onMouseOver() {
+        this.setState({
+            onHover: true
+        });
+    }
 
-  onMouseOut () {
-      this.setState({
-          onHover: false
-      });
-  }
+    onMouseOut() {
+        this.setState({
+            onHover: false
+        });
+    }
 
-  render() {
-    const { classes } = this.props;
-    const { onHover } = this.state;
+    render() {
+        const { classes, name, defaultValue, changeData } = this.props;
+        const { onHover } = this.state;
 
-    return (
-      <Container className={classes.textFieldContainer}>
-          <Typography className={classes.title}>
-              {this.props.name}
-          </Typography>
-        <TextField
-          onMouseOver={this.onMouseOver}
-          onMouseOut={this.onMouseOut}
-          id={this.props.name}
-          name={this.props.name}
-          variant="outlined"
-          defaultValue={this.props.default}
-          className={classes.textField}
-          InputProps={{
-            className: classes.input,
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  disabled
-                  edge="start"
-                >
-                  {onHover && <EditRoundedIcon style={{margin:'-12px'}}/>}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          onChange={this.props.changeData}
-        />
-      </Container>
-      );
-  }
+        return (
+              <Container className={classes.textFieldContainer}>
+                    <Typography className={classes.title}>
+                        {name}
+                    </Typography>
+                    <TextField
+                        onMouseOver={this.onMouseOver}
+                        onMouseOut={this.onMouseOut}
+                        id={name}
+                        name={name}
+                        variant="outlined"
+                        defaultValue={defaultValue}
+                        className={classes.textField}
+                        InputProps={{
+                            className: classes.input,
+                            endAdornment: (
+                                <InputAdornment position="end">
+                                    <IconButton
+                                      disabled
+                                      edge="start"
+                                    >
+                                        {onHover && <EditRoundedIcon style={{margin:'-12px'}}/>}
+                                    </IconButton>
+                                </InputAdornment>
+                            )
+                        }}
+                        onChange={changeData}
+                    />
+              </Container>
+          );
+    }
 }
 
 export default withStyles(styles)(SettingNormalTextField);
--- front-end/src/components/TextField/SettingPasswordTextField.js
+++ front-end/src/components/TextField/SettingPasswordTextField.js
@@ -14,131 +14,131 @@ import VisibilityOff from '@material-ui/icons/VisibilityOff';
 
 
 const styles = theme => ({
-  textFieldContainer: {
-    marginLeft: "1.75%",
-    marginRight: "1.75%",
-  },
-  textField: {
-    width: '100%',
-    marginTop: "5px",
-    fontWeight: "normal",
-    "& label.Mui-focused": {
-      color: "black"
+    textFieldContainer: {
+        minWidth: '150px',
+        marginLeft: "1.75%",
+        marginRight: "1.75%",
     },
-    "& .MuiFilledInput-underline:after": {
-      borderBottom: "2px solid #000000"
+    textField: {
+        width: '100%',
+        marginTop: "5px",
+        fontWeight: "normal",
+        "& label.Mui-focused": {
+            color: "black"
+        },
+        "& .MuiFilledInput-underline:after": {
+            borderBottom: "2px solid #000000"
+        },
+        "& .MuiFilledInput-root": {
+            "&.Mui-focused": {
+                backgroundColor: "rgba(225,225,225,0.5)"
+            }
+        },
     },
-    "& .MuiFilledInput-root": {
-      "&.Mui-focused": {
-        backgroundColor: "rgba(225,225,225,0.5)"
-      }
+    input: {
+        color: "black",
+        backgroundColor: "rgba(225,225,225,0.65)",
+        "&:hover": {
+            backgroundColor: "rgba(225,225,225,0.5)"
+        },
+        "& input": {
+            backgroundColor: "rgba(225,225,225,0)"
+        },
+        fontSize: 'medium',
+        [theme.breakpoints.down('xs')]: {
+            fontSize: 'small',
+            height: '40px'
+        },
     },
-  },
-  input: {
-    color: "black",
-    backgroundColor: "rgba(225,225,225,0.65)",
-    "&:hover": {
-      backgroundColor: "rgba(225,225,225,0.5)"
+    title: {
+        fontSize: '20px',
+        [theme.breakpoints.down('xs')]: {
+            fontSize: '15px'
+        },
+        paddingLeft: '5px',
+        fontWeight: "bold",
     },
-    "& input": {
-      backgroundColor: "rgba(225,225,225,0)"
-    },
-    fontSize: 'medium',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: 'small',
-      height: '40px'
-    },
-  },
-  title: {
-    fontSize: '20px',
-    [theme.breakpoints.down('xs')]: {
-      fontSize: '15px'
-    },
-    paddingLeft: '3%',
-    fontWeight: "bold",
-  },
 });
 
 class SettingNormalTextField extends React.Component {
-  constructor(props) {
-      super(props);
-      this.state = {
-          onHover: false,
-          visibility: false,
-          invalid: false,
-      };
-      this.onMouseOver = this.onMouseOver.bind(this);
-      this.onMouseOut = this.onMouseOut.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            onHover: false,
+            visibility: false,
+            invalid: false,
+        };
+        this.onMouseOver = this.onMouseOver.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+    }
 
-  onMouseOver () {
-      this.setState({
-          onHover: true
-      });
-  }
+    onMouseOver() {
+        this.setState({
+            onHover: true
+        });
+    }
 
-  onMouseOut () {
-      this.setState({
-          onHover: false
-      });
-  }
+    onMouseOut() {
+        this.setState({
+            onHover: false
+        });
+    }
 
-  seePassword = () => {
-      this.setState({
-          visibility: !this.state.visibility
-      })
-  }
+    seePassword = () => {
+        this.setState({
+            visibility: !this.state.visibility
+        })
+    }
 
-  mouseDownPassword = (event) => {
-      event.preventDefault();
-  }
+    mouseDownPassword = (event) => {
+        event.preventDefault();
+    }
 
-  render() {
-    const { classes } = this.props;
-    const { onHover } = this.state;
+    render() {
+        const { classes, name, invalid, helper, changeData } = this.props;
+        const { onHover, visibility } = this.state;
 
-    return (
-      <Container className={classes.textFieldContainer}>
-           <Typography className={classes.title}>
-               {this.props.name}
-           </Typography>
-        <TextField
-          onMouseOver={this.onMouseOver}
-          onMouseOut={this.onMouseOut}
-          id={this.props.name}
-          name={this.props.name}
-          variant="outlined"
-          type={!this.state.visibility? "password":"text"}
-          error={this.state.invalid}
-          helperText={this.state.invalid && "Incorrect password."}
-          className={classes.textField}
-          InputProps={{
-            className: classes.input,
-            endAdornment: (
-              <InputAdornment position="end">
-                <IconButton
-                  disabled
-                  edge="start"
-                >
-                  {onHover && <EditRoundedIcon style={{margin:'-30px'}}/>}
-                  
-                </IconButton>
-                <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={this.seePassword}
-                    onMouseDown={this.mouseDownPassword}
-                    edge="end"
-                >
-                    {this.state.visibility? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            )
-          }}
-          onChange={this.props.changeData}
-        />
-      </Container>
-      );
-  }
+        return (
+            <Container className={classes.textFieldContainer}>
+                <Typography className={classes.title}>
+                    {name}
+                </Typography>
+                <TextField
+                    onMouseOver={this.onMouseOver}
+                    onMouseOut={this.onMouseOut}
+                    id={name}
+                    name={name}
+                    variant="outlined"
+                    type={!visibility? "password":"text"}
+                    error={invalid}
+                    helperText={invalid && helper}
+                    className={classes.textField}
+                    InputProps={{
+                        className: classes.input,
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <IconButton
+                                    disabled
+                                    edge="start"
+                                >
+                                    {onHover && <EditRoundedIcon style={{margin:'-10px'}}/>}
+                                </IconButton>
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={this.seePassword}
+                                    onMouseDown={this.mouseDownPassword}
+                                    edge="end"
+                                >
+                                    {visibility? <Visibility /> : <VisibilityOff />}
+                                </IconButton>
+                          </InputAdornment>
+                        )
+                    }}
+                    onChange={changeData}
+                />
+            </Container>
+          );
+    }
 }
 
 export default withStyles(styles)(SettingNormalTextField);
