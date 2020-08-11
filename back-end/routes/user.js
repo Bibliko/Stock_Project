ebdb6ba47c3520425fb26e0b4c82525aec25da0a
HuynhHoangPhuc <phuchoang.huynh.207@gmail.com>
2020-08-11 19:54:06 +0700

    Update ranking back-end

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -4,6 +4,8 @@ const router = Router();
 const prisma = new PrismaClient();
 // const { indices } = require('../algolia');
 
+const { keysAsync, getAsync } = require("../redis/redis-client");
+
 router.put("/changeData", (req, res) => {
   const { dataNeedChange, email } = req.body;
 
@@ -71,6 +73,61 @@ router.get("/getData", (req, res) => {
       console.log(err);
       res.status(500).send("Get data of user fails.");
     });
+});
+
+router.get("/getOverallRanking", async (_, res) => {
+  try {
+    const rankingList = await keysAsync("RANKING*");
+    let usersRankingList = new Array(rankingList.length);
+    await Promise.all(rankingList.map(async (v) => {
+      try {
+        const getUser = await getAsync(v);
+        const user = getUser.split("&");
+        usersRankingList[parseInt(v.slice(7)) - 1] = {
+          firstName: user[0],
+          lastName: user[1],
+          totalPortfolio: parseInt(user[2]),
+          region: user[3]
+        };
+      } catch (err) {
+        console.log(err);
+      }
+    }));
+    res.send(usersRankingList);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Get overall ranking fails.");
+  }
+});
+
+router.get("/getRegionalRanking", async (req, res) => {
+  const {region} = req.query;
+
+  try {
+    const rankingList = await keysAsync("RANKING*");
+    let usersRankingList = new Array(rankingList.length);
+    await Promise.all(rankingList.map(async (v) => {
+      try {
+        const getUser = await getAsync(v);
+        const user = getUser.split("&");
+        if (region === user[3]) {
+          usersRankingList[parseInt(v.slice(7)) - 1] = {
+            firstName: user[0],
+            lastName: user[1],
+            totalPortfolio: parseInt(user[2]),
+            region: user[3]
+          };
+        }
+
+      } catch (err) {
+        console.log(err);
+      }
+    }));
+    res.send(usersRankingList.filter(user => user !== null));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Get overall ranking fails.");
+  }
 });
 
 module.exports = router;
