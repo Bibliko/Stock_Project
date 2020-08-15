3b7e0468dacf622662356a3d45cea23b6d5f1b7b
HuynhHoangPhuc <phuchoang.huynh.207@gmail.com>
2020-08-15 09:47:34 +0700

    Change to Promise

--- back-end/routes/user.js
+++ back-end/routes/user.js
@@ -75,59 +75,61 @@ router.get("/getData", (req, res) => {
     });
 });
 
-router.get("/getOverallRanking", async (_, res) => {
-  try {
-    const rankingList = await keysAsync("RANKING*");
-    let usersRankingList = new Array(rankingList.length);
-    await Promise.all(rankingList.map(async (v) => {
-      try {
-        const getUser = await getAsync(v);
-        const user = getUser.split("&");
-        usersRankingList[parseInt(v.slice(7)) - 1] = {
-          firstName: user[0],
-          lastName: user[1],
-          totalPortfolio: parseInt(user[2]),
-          region: user[3]
-        };
-      } catch (err) {
-        console.log(err);
-      }
-    }));
-    res.send(usersRankingList);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Get overall ranking fails.");
-  }
+router.get("/getOverallRanking", (_, res) => {
+  keysAsync("RANKING|*")
+    .then((keysList) => {
+      let usersRankingList = keysList.map((key) => {
+        return getAsync(key)
+          .then((user) => {
+            const getUser = user.split("|");
+            return {
+              firstName: getUser[0],
+              lastName: getUser[1],
+              totalPortfolio: parseInt(getUser[2]),
+              region: getUser[3]
+            };
+          });
+      });
+      return Promise.all(usersRankingList);
+    })
+    .then((usersList) => {
+      res.send(usersList);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Get overall ranking fails.");
+    })
 });
 
-router.get("/getRegionalRanking", async (req, res) => {
+router.get("/getRegionalRanking", (req, res) => {
   const {region} = req.query;
 
-  try {
-    const rankingList = await keysAsync("RANKING*");
-    let usersRankingList = new Array(rankingList.length);
-    await Promise.all(rankingList.map(async (v) => {
-      try {
-        const getUser = await getAsync(v);
-        const user = getUser.split("&");
-        if (region === user[3]) {
-          usersRankingList[parseInt(v.slice(7)) - 1] = {
-            firstName: user[0],
-            lastName: user[1],
-            totalPortfolio: parseInt(user[2]),
-            region: user[3]
-          };
-        }
-
-      } catch (err) {
-        console.log(err);
-      }
-    }));
-    res.send(usersRankingList.filter(user => user !== null));
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Get overall ranking fails.");
-  }
+  keysAsync("RANKING|*")
+    .then((keysList) => {
+      let usersRankingList = keysList.map((key) => {
+        return getAsync(key)
+          .then((user) => {
+            const getUser = user.split("|");
+            if (getUser[3] === region) {
+              return {
+                firstName: getUser[0],
+                lastName: getUser[1],
+                totalPortfolio: parseInt(getUser[2]),
+                region: getUser[3]
+              };
+            }
+            return 0;
+          });
+      });
+      return Promise.all(usersRankingList);
+    })
+    .then((usersList) => {
+      res.send(usersList.filter(user => user !== 0));
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Get overall ranking fails.");
+    })
 });
 
 module.exports = router;
