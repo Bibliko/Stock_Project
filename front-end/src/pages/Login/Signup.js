1a442b0d3a11ab4612ff1fc308a04187626d9b9e
Tu Do <doanhtu07@gmail.com>
2020-08-16 00:17:52 -0500

    Fix small things

--- front-end/src/pages/Login/Signup.js
+++ front-end/src/pages/Login/Signup.js
@@ -1,290 +1,294 @@
-import React from 'react';
-import { isEmpty, isEqual } from 'lodash';
-import { withRouter } from 'react-router';
-import { connect } from 'react-redux';
+import React from "react";
+import { isEmpty, isEqual } from "lodash";
+import { withRouter } from "react-router";
+import { connect } from "react-redux";
 
-import { shouldRedirectToLandingPage, redirectToPage } from '../../utils/PageRedirectUtil';
-import { signupUser } from '../../utils/UserUtil';
+import {
+  shouldRedirectToLandingPage,
+  redirectToPage,
+} from "../../utils/PageRedirectUtil";
+import { signupUser } from "../../utils/UserUtil";
 
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core/styles';
-import NormalTextField from '../../components/TextField/normalTextField';
-import PasswordTextField from '../../components/TextField/passwordTextField';
-import { Container } from '@material-ui/core';
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from "@material-ui/core/styles";
+import NormalTextField from "../../components/TextField/NormalTextField";
+import PasswordTextField from "../../components/TextField/PasswordTextField";
+import { Container } from "@material-ui/core";
 
-const styles = theme => ({
-    root: {
-        position: 'absolute',
-        height: '100vh',
-        width: '100vw',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        maxWidth: 'none',
-        minHeight: '605px'
+const styles = (theme) => ({
+  root: {
+    position: "absolute",
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    maxWidth: "none",
+    minHeight: "605px",
+  },
+  paper: {
+    height: "fit-content",
+    minHeight: "550px",
+    width: "fit-content",
+    minWidth: "450px",
+    [theme.breakpoints.down("xs")]: {
+      height: "-webkit-fill-available",
+      width: "-webkit-fill-available",
+      minWidth: 0,
     },
-    paper: {
-        height: 'fit-content',
-        minHeight: '550px',
-        width: 'fit-content',
-        minWidth: '450px',
-        [theme.breakpoints.down('xs')]: {
-            height: '-webkit-fill-available',
-            width: '-webkit-fill-available',
-            minWidth: 0,
-        },
-        padding: theme.spacing(1),
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        background: theme.palette.gradientPaper.main,
+    padding: theme.spacing(1),
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    background: theme.palette.gradientPaper.main,
+  },
+  div: {
+    background: "black",
+    backgroundSize: "cover",
+    height: "100vh",
+    width: "100vw",
+    position: "fixed",
+  },
+  center: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    margin: "auto",
+    flexBasis: "unset",
+  },
+  title: {
+    fontSize: "x-large",
+    fontWeight: "bold",
+  },
+  avatar: {
+    height: "130px",
+    width: "130px",
+    margin: theme.spacing(3),
+  },
+  submit: {
+    marginTop: "10px",
+    padding: theme.spacing(1),
+    height: "40px",
+    width: "120px",
+    background: "black",
+    "&:hover": {
+      backgroundColor: "black",
+      opacity: 0.8,
     },
-    div: {
-        background: 'black',
-        backgroundSize: 'cover',
-        height: '100vh',
-        width: '100vw',
-        position: 'fixed'
-    },
-    center: {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 'auto',
-        flexBasis: 'unset'
-    },
-    title: {
-        fontSize: 'x-large',
-        fontWeight: 'bold'
-    },
-    avatar: {
-        height: '130px',
-        width: '130px',
-        margin: theme.spacing(3)
-    },
-    submit: {
-        marginTop: '10px',
-        padding: theme.spacing(1),
-        height: '40px',
-        width: '120px',
-        background: 'black',
-        '&:hover': {
-            backgroundColor: 'black',
-            opacity: 0.8
-        },
-        borderRadius: '40px',
-        color: 'white',
-        fontWeight: 'bold'
-    },
-    link: {
-        marginTop: '6px',
-        backgroundColor: 'transparent',
-        color: 'black',
-        fontWeight: 'bold',
-        textTransform: 'none',
-        fontSize: '16px'
-    },
-    announcement: {
-        marginTop: 5,
-        display: 'flex',
-        justifyContent: 'center',
-    },
-    announcementText: {
-        fontSize: 'small'
-    },
+    borderRadius: "40px",
+    color: "white",
+    fontWeight: "bold",
+  },
+  link: {
+    marginTop: "6px",
+    backgroundColor: "transparent",
+    color: "black",
+    fontWeight: "bold",
+    textTransform: "none",
+    fontSize: "16px",
+  },
+  announcement: {
+    marginTop: 5,
+    display: "flex",
+    justifyContent: "center",
+  },
+  announcementText: {
+    fontSize: "small",
+  },
 });
 
 class Signup extends React.Component {
-    state = {
-        error: "",
-        success: "",
+  state = {
+    error: "",
+    success: "",
+  };
+
+  errorTypes = [
+    "Confirm Password does not match Password.",
+    "Missing some fields",
+  ];
+
+  email = "";
+  password = "";
+  confirmPassword = "";
+
+  setStateErrorPassword = () => {
+    if (!isEqual(this.password, this.confirmPassword)) {
+      if (!isEqual(this.state.error, this.errorTypes[0])) {
+        this.setState({ error: this.errorTypes[0] });
+      }
+    } else {
+      if (!isEmpty(this.state.error)) {
+        this.setState({ error: "" });
+      }
+    }
+  };
+
+  changeEmail = (event) => {
+    this.email = event.target.value;
+    if (
+      isEqual(this.password, this.confirmPassword) &&
+      !isEmpty(this.state.error)
+    ) {
+      this.setState({ error: "" });
+    }
+  };
+
+  changePassword = (event) => {
+    this.password = event.target.value;
+    this.setStateErrorPassword();
+  };
+
+  changeConfirmPassword = (event) => {
+    this.confirmPassword = event.target.value;
+    this.setStateErrorPassword();
+  };
+
+  submit = () => {
+    if (
+      isEmpty(this.email) ||
+      isEmpty(this.password) ||
+      isEmpty(this.confirmPassword)
+    ) {
+      this.setState({ error: this.errorTypes[1] });
+    } else {
+      if (isEmpty(this.state.error)) {
+        signupUser({
+          email: this.email,
+          password: this.password,
+        })
+          .then((res) => {
+            this.setState({ success: res });
+          })
+          .catch((err) => {
+            this.setState({ error: err });
+          });
+      }
+    }
+  };
+
+  handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      this.submit();
+    }
+  };
+
+  componentDidMount() {
+    if (shouldRedirectToLandingPage(this.props)) {
+      redirectToPage("/", this.props);
+    }
+  }
+
+  componentDidUpdate() {
+    if (shouldRedirectToLandingPage(this.props)) {
+      redirectToPage("/", this.props);
+    }
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    if (shouldRedirectToLandingPage(this.props)) {
+      return null;
     }
 
-    errorTypes = [
-        "Confirm Password does not match Password.",
-        "Missing some fields"
-    ]
-    
-    email=""
-    password=""
-    confirmPassword=""
-
-    setStateErrorPassword = () => {
-        if(!isEqual(this.password, this.confirmPassword)) {
-
-            if(!isEqual(this.state.error, this.errorTypes[0])) {
-                this.setState({ error: this.errorTypes[0] });
-            }
-        }
-        else {
-            if(!isEmpty(this.state.error)) {
-                this.setState({ error: "" });
-            }
-        }
-    }
-
-    changeEmail = (event) => {
-        this.email = event.target.value;
-        if(
-            isEqual(this.password, this.confirmPassword) &&
-            !isEmpty(this.state.error)
-        ) {
-            this.setState({ error: "" });
-        }
-    }
-
-    changePassword = (event) => {
-        this.password = event.target.value;
-        this.setStateErrorPassword();
-    }
-
-    changeConfirmPassword = (event) => {
-        this.confirmPassword=event.target.value;
-        this.setStateErrorPassword();
-    }
-
-    submit = () => {
-        if(
-            isEmpty(this.email) ||
-            isEmpty(this.password) ||
-            isEmpty(this.confirmPassword)
-        ) {
-            this.setState({ error: this.errorTypes[1] }); 
-        }
-        else {
-            if(isEmpty(this.state.error)) {
-                signupUser({
-                    email: this.email,
-                    password: this.password
-                })
-                .then(res => {
-                    this.setState({ success: res });
-                })
-                .catch(err => {
-                    this.setState({ error: err });
-                })
-            }
-        }
-    }
-
-    handleKeyDown = (event) => {
-        if(event.key==="Enter") {
-            this.submit();
-        }
-    }
-    
-    componentDidMount() {
-        if(shouldRedirectToLandingPage(this.props)) {
-            redirectToPage('/', this.props);
-        }
-    }
-
-    componentDidUpdate() {
-        if(shouldRedirectToLandingPage(this.props)) {
-            redirectToPage('/', this.props);
-        }
-    }
-
-    render() {
-        const { classes } = this.props;
-
-        if (shouldRedirectToLandingPage(this.props)) {
-            return null;
-        }
-
-        return (
-            <div>
-                <div className={classes.div}/>
-                <Container className={classes.root} disableGutters>
-                    <Paper 
-                        className={classes.paper}
-                        elevation={2}
+    return (
+      <div>
+        <div className={classes.div} />
+        <Container className={classes.root} disableGutters>
+          <Paper className={classes.paper} elevation={2}>
+            <Grid
+              container
+              spacing={1}
+              direction="column"
+              className={classes.center}
+            >
+              <Grid item xs className={classes.center}>
+                <img
+                  src="/bibOfficial.jpg"
+                  alt="Bibliko"
+                  className={classes.avatar}
+                />
+              </Grid>
+              <Grid
+                container
+                spacing={2}
+                direction="column"
+                item
+                xs
+                className={classes.center}
+              >
+                <Grid item xs className={classes.center}>
+                  <NormalTextField
+                    name="Email"
+                    changeData={this.changeEmail}
+                    enterData={this.handleKeyDown}
+                  />
+                </Grid>
+                <Grid item xs className={classes.center}>
+                  <PasswordTextField
+                    name="Password"
+                    changePassword={this.changePassword}
+                    enterPassword={this.handleKeyDown}
+                  />
+                </Grid>
+                <Grid item xs className={classes.center}>
+                  <PasswordTextField
+                    name="Confirm Password"
+                    changePassword={this.changeConfirmPassword}
+                    enterPassword={this.handleKeyDown}
+                  />
+                </Grid>
+                {!isEmpty(this.state.error) && (
+                  <Grid item xs className={classes.announcement}>
+                    <Typography
+                      color="error"
+                      align="center"
+                      className={classes.announcementText}
                     >
-                        <Grid container spacing={1} direction="column"
-                            className={classes.center}
-                        >
-                            <Grid item xs className={classes.center}>
-                                <img 
-                                    src="/bibOfficial.jpg"
-                                    alt="Bibliko"
-                                    className={classes.avatar}
-                                />
-                            </Grid>
-                            <Grid 
-                                container spacing={2} direction="column"
-                                item xs className={classes.center}
-                            >
-                                <Grid item xs className={classes.center}>
-                                    <NormalTextField
-                                        name= 'Email'
-                                        changeData={this.changeEmail}
-                                        enterData={this.handleKeyDown}
-                                    />
-                                </Grid>
-                                <Grid item xs className={classes.center}>
-                                    <PasswordTextField 
-                                        name='Password'
-                                        changePassword={this.changePassword}
-                                        enterPassword={this.handleKeyDown}
-                                    />
-                                </Grid>
-                                <Grid item xs className={classes.center}>
-                                    <PasswordTextField
-                                        name='Confirm Password'
-                                        changePassword={this.changeConfirmPassword}
-                                        enterPassword={this.handleKeyDown}
-                                    />
-                                </Grid>
-                                {
-                                    !isEmpty(this.state.error) &&
-                                    <Grid item xs className={classes.announcement}>
-                                        <Typography color="error" align="center"
-                                            className={classes.announcementText}
-                                        >
-                                            Error: {this.state.error}
-                                        </Typography>
-                                    </Grid>
-                                }
-                                {
-                                    !isEmpty(this.state.success) &&
-                                    <Grid item xs className={classes.announcement}>
-                                        <Typography color="primary" align="center"
-                                            className={classes.announcementText}
-                                        >
-                                            Success: {this.state.success}
-                                        </Typography>
-                                    </Grid>
-                                }
-                                <Grid item xs className={classes.center}>
-                                    <Button className={classes.submit}
-                                        onClick={this.submit}
-                                    >
-                                        Sign Up
-                                    </Button>
-                                    <Button 
-                                        color="primary"
-                                        onClick={() => {redirectToPage("/login", this.props)}}
-                                        className={classes.link}
-                                    >
-                                        Back to Login
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Paper>
-                </Container>
-            </div>
-        );
-    }
+                      Error: {this.state.error}
+                    </Typography>
+                  </Grid>
+                )}
+                {!isEmpty(this.state.success) && (
+                  <Grid item xs className={classes.announcement}>
+                    <Typography
+                      color="primary"
+                      align="center"
+                      className={classes.announcementText}
+                    >
+                      Success: {this.state.success}
+                    </Typography>
+                  </Grid>
+                )}
+                <Grid item xs className={classes.center}>
+                  <Button className={classes.submit} onClick={this.submit}>
+                    Sign Up
+                  </Button>
+                  <Button
+                    color="primary"
+                    onClick={() => {
+                      redirectToPage("/login", this.props);
+                    }}
+                    className={classes.link}
+                  >
+                    Back to Login
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Container>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => ({
-    userSession: state.userSession
+  userSession: state.userSession,
 });
 
-export default connect(mapStateToProps)(
-    withStyles(styles)(withRouter(Signup))
-);
+export default connect(mapStateToProps)(withStyles(styles)(withRouter(Signup)));
