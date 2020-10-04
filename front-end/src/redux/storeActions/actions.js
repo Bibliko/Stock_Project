7e5c6e26c892f27d93468c16d34deabb6aa7cfb1
mqluu02 <65616182+mqluu02@users.noreply.github.com>
2020-10-04 11:19:21 -0600

    Update actions.js

--- front-end/src/redux/storeActions/actions.js
+++ front-end/src/redux/storeActions/actions.js
@@ -1,6 +1,25 @@
 import actionTypes from './actionTypes';
+import Axios from 'axios'
 
 // These actions is used for dispatch function when Redux mapDispatchToProps is used in components
+
+export const fetchUserRegionalRanking= ()=> async dispatch =>{
+  const res= await Axios.get('/api/userData/getRegionalRanking')
+  dispatch ({
+    type: actionTypes.USERREGIONALRANKING,
+    payload: res.data
+  })
+
+}
+
+export const fetchUserOverallRanking = ()=> async dispatch => {
+  const res= await  Axios.get('api/userData/getOverallRanking');
+
+  dispatch ({
+    type: actionTypes.USERRANKING,
+    payload: res.data
+  });
+};
 
 export function userAction(method, whichPropsToChangeOrWhatDataForChanging) {
     return {
